678ff0a7ed4556dbf9eac6e198fadeb23a1cc1a5
alexei379 <alexei379@gmail.com>
2018-03-01 21:22:56 -0800

    Cleanup

--- Thresholder.py
+++ Thresholder.py
@@ -24,40 +24,6 @@ class Thresholder():
         # binary_output = np.copy(img) # Remove this line
         return binary_output
 
-    def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
-        # Apply the following steps to img
-        # 1) Convert to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        # 2) Take the gradient in x and y separately
-        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-        # 3) Calculate the magnitude
-        gradmag = np.sqrt(sobelx ** 2 + sobely ** 2)
-        # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
-        scale_factor = np.max(gradmag) / 255
-        gradmag = (gradmag / scale_factor).astype(np.uint8)
-        # 5) Create a binary mask where mag thresholds are met
-        binary_output = np.zeros_like(gradmag)
-        binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
-        # 6) Return this mask as your binary_output image
-        return binary_output
-
-    @staticmethod
-    def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi / 2)):
-        # Apply the following steps to img
-        # 1) Convert to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        # 2) Take the gradient in x and y separately
-        sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-        sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-        # 3) Take the absolute value of the x and y gradients
-        # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
-        absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
-        # 5) Create a binary mask where direction thresholds are met
-        binary_output = np.zeros_like(absgraddir)
-        binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
-        # 6) Return this mask as your binary_output image
-        return binary_output
 
     @staticmethod
     def color_threshold(img, s_thresh=(0, 255), v_thresh=(0, 255)):
